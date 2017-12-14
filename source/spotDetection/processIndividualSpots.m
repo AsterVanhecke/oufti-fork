a589282f7e77fa28dc72db378650721639cb29b8
AsterVanhecke <aster.vanhecke@gmail.com>
2017-12-14 12:31:19 +0100

    Implemented background subtraction in processIndividualSpots
    Also added some comments

--- source/spotDetection/processIndividualSpots.m
+++ source/spotDetection/processIndividualSpots.m
@@ -25,34 +25,26 @@ function [spotStructure, newImage,dispStructure] = processIndividualSpots(cellDa
 %Note:  No global variables are allowed in this function
 %--------------------------------------------------------------------------------------
 
-
-%diamond = strel('square',5);
 try
+ %% Prepare variables and parameters
  spotStructure.l                            = [];
  spotStructure.d                            = [];
  spotStructure.x                            = [];
  spotStructure.y                            = [];
  spotStructure.positions                    = [];
- spotStructure.adj_Rsquared                     = [];
- dispStructure.adj_Rsquared                     = [];
+ spotStructure.adj_Rsquared                 = [];
+ dispStructure.adj_Rsquared                 = [];
  dispStructure.w                            = [];
  dispStructure.h                            = [];
 if (isempty(cellData) || length(cellData.mesh) < 4),return; end
-newImage = [];
-
-scale             = params.scale; %Maximum radius from a peak to consider for fitting
-sigmaPsf          = params.spotRadius; %Radius that other spots should be blocked from a spot currently being fitted
-minThresh         = params.int_threshold;     
-lowPass           = params.lowPass;
 rawImage = imcrop(image,cellData.box);
-bgr = mean(mean(image));
+% bgr = mean(mean(image));
 [rows,columns] = size(rawImage);
 cellContour = cat(1,cellData.mesh(:,1:2),flipud(cellData.mesh(:,3:4))) + 1;
 %subtract off box, one dimension at a time to avoid using repmat
 cellContour(:,1) = cellContour(:,1) - cellData.box(1);
 cellContour(:,2) = cellContour(:,2) - cellData.box(2);
 cellMask = single(poly2mask(double(cellContour(:,1)),double(cellContour(:,2)),rows,columns));
-% % % backgroundRawImage = max(mean(mean(rawImage)),0);
 %pad the cell mask with ones -- this method is WAY (~100x) faster than the
 %built in f'n imdilate for this purpose
 dilatedCellMask = conv2(cellMask,ones(3),'same');
@@ -64,51 +56,52 @@ dilatedCellContour = bwtraceboundary(dilatedCellMask,[dilatedCellContourX,dilate
 dilatedCellContour = frdescp(dilatedCellContour);
 dilatedCellContour = ifdescp(dilatedCellContour,20);
 dilatedCellContour = [dilatedCellContour(:,2),dilatedCellContour(:,1)];
-  if scale == 0
-        newImage = bandPassFilter(rawImage,1,sigmaPsf,minThresh);
-  else
-      newImage1 = atrouswave(rawImage,scale,lowPass,7,sigmaPsf);
-      newImage = bandPassFilter(newImage1,1,sigmaPsf,minThresh);
-  end
-  maskOfNewImage = ones(size(newImage),'single');
-  maskOfNewImage = newImage.*maskOfNewImage;
-% % %   maskOfNewImage = imopen(maskOfNewImage,ones(3));
-  maskOfNewImage = maskOfNewImage > 0;
-  newRawImage = im2uint16(maskOfNewImage.*im2double(rawImage));
-%identify background pixels as pixels inside the convoloved (dilated) cell,
-%masked out by bandpass filtering
-% % % newImageResultant = newImage-newImage1;
-backgroundPixels = ~(logical(conv2(newImage,ones(3),'same')))+~dilatedCellMask;
-backgroundPixels1 = backgroundPixels(:) == 2;
-backgroundPixels2 = backgroundPixels(:) == 1;
-backgroundPixels = mode(single([backgroundPixels1 backgroundPixels2]));
-backgroundPixelsValues = rawImage(logical(backgroundPixels));
-numberOfSpots = bwconncomp(newRawImage,8);
+
+%% Detect spots to create mask - Optional
+[~,~,maskOfNewImage]=spotDetection(rawImage,params);
+
+%% Background subtraction
+bgrDark=median(image(:));
+out=BG_subtraction_points(cellData,rawImage,bgrDark,params.spotRadius,maskOfNewImage); % mask out spots
+% out=BG_subtraction_points(cellData,rawImage,align,PSFwidth,bgrDark); % dont mask out spots
+BGsubImage=out.bg_subtracted;
+% out.backgroundImage=zeros(size(rawImage)); % no BG subtraction
+
+%% Redo spot detection (test if this improves spot detection)
+[numberOfSpots,newRawImage,~,newImage]=spotDetection(BGsubImage,params); % BG subtraction
+% [numberOfSpots,newRawImage,~]=spotDetection(rawImage,params); % no BG subtraction
 
 if isempty(numberOfSpots), return; end
 
-[spotStructure,dispStructure] = fitGaussians(cellData,numberOfSpots,rawImage,newRawImage,bgr,params,dilatedCellContour);
-% % % % spotStructure = fitGaussiansFinal(cellData,spotStructure,rawImage,params,dilatedCellContour);
+[spotStructure,dispStructure] = fitGaussians(cellData,numberOfSpots,BGsubImage,newRawImage,bgrDark,params,dilatedCellContour); % BG subtraction
+% [spotStructure,dispStructure] = fitGaussians(cellData,numberOfSpots,rawImage,newRawImage,bgrDark,params,dilatedCellContour); % no BG subtraction
+dispStructure.rawImage=rawImage;
+dispStructure.BGsubImage=BGsubImage;
+dispStructure.backgroundImage=out.backgroundImage;
 
 catch err
      disp(['Error in ' err.stack(2).file ' in line ' num2str(err.stack(2).line)]);
      disp(err.message);
      return;
 end
-
 end
 
 
+function [numberOfSpots,newRawImage,maskOfNewImage,newImage]=spotDetection(rawImage,params)
+scale             = params.scale; %Maximum radius from a peak to consider for fitting
+sigmaPsf          = params.spotRadius; %Radius that other spots should be blocked from a spot currently being fitted
+minThresh         = params.int_threshold;     
+lowPass           = params.lowPass;
+if scale == 0
+    newImage = bandPassFilter(rawImage,1,sigmaPsf,minThresh);
+else
+    newImage1 = atrouswave(rawImage,scale,lowPass);
+    newImage = bandPassFilter(newImage1,1,sigmaPsf,minThresh);
+end
+maskOfNewImage = ones(size(newImage),'single');
+maskOfNewImage = newImage.*maskOfNewImage;
+maskOfNewImage = maskOfNewImage > 0;
+newRawImage = im2uint16(maskOfNewImage.*im2double(rawImage));
 
-
-
-
-
-
-
-
-
-
-
-
-
+numberOfSpots = bwconncomp(newRawImage,8);
+end
