706e23bc49df3651faf1e83e16fff28ced2acce4
AsterVanhecke <aster.vanhecke@gmail.com>
2018-02-13 18:08:58 +0100

    kymographFcn cell range and saving
    Extended kymograph functionality
    It now accepts more than one cell, even a range of cells.
    It will add the cell number in the figure title.
    It can also save the figures if a folder is specified.

--- source/stat/kymographFcn.m
+++ source/stat/kymographFcn.m
@@ -27,7 +27,7 @@ screenSize = get(0,'ScreenSize');
 
 if sum(cellfun(@numel,cellList.meshData)) == 0,warndlg('First load cell meshes'),return;end
 
-input = inputdlg({'cell number','conversion factor from frame to time','conversion factor from pixels to microns (leave blank if not known)','frame range ([1 10])','signal (signal1 or signal2)'});
+input = inputdlg({'cell number','conversion factor from frame to time','conversion factor from pixels to microns (leave blank if not known)','frame range ([1 10])','signal (signal1 or signal2)', 'Folder for saving?'});
 if isempty(input),return;end
 try
     % R2010a and newer
@@ -51,14 +51,22 @@ javacomponent(jObj.getComponent, [1,1,pos(3),pos(4)],hh);
 pause(0.01);drawnow;
 jObj.start;
     % do some long operation...
-try
-    figure('name','kymograph','NumberTitle','off');
-    if isempty(input{3})
-        kymograph(cellList,str2double(input{1}),50,str2double(input{2}),[],str2num(input{4}),input{5});
-    else
-        kymograph(cellList,str2double(input{1}),50,str2double(input{2}),str2double(input{3}),str2num(input{4}),input{5});
+for cellIdx=str2num(input{1})
+    try
+        figure('name','kymograph','NumberTitle','off');
+        title(['kymograph for cell ' num2str(cellIdx)]), hold on
+        if isempty(input{3})
+            kymograph(cellList,cellIdx,50,str2double(input{2}),[],str2num(input{4}),input{5});
+        else
+            kymograph(cellList,cellIdx,50,str2double(input{2}),str2double(input{3}),str2num(input{4}),input{5});
+        end
+        if ~isempty(input{6})
+            saveas(gcf,[input{6} 'kymo_cell_' num2str(cellIdx) '_' input{5} '.png'])
+            saveas(gcf,[input{6} 'kymo_cell_' num2str(cellIdx) '_' input{5} '.fig'])
+            close gcf
+        end
+    catch
     end
-catch
 end
 jObj.stop;
 delete(hh)
